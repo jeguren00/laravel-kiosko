38dd21f35e1c095a40e66aad707e92983a9616d2
jordi <jordi@DESKTOP-JORDI.home>
2024-01-11 22:15:44 +0100

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Database\Seeders\ProductoSeeder;
+use Database\Seeders\CategoriaSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call(CategoriaSeeder::class);
+        $this->call(ProductoSeeder::class);
+    }
+}
